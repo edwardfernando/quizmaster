094f249c26508263fd12816ee5e9774a3afc58f2
Edward Fernando <edward.fer@gmail.com>
2017-08-14 22:18:01 +0700

    Question controller + rspec

--- app/controllers/api/quiz/questions_controller.rb
+++ app/controllers/api/quiz/questions_controller.rb
@@ -1,24 +1,24 @@
-class Api::Quiz::QuestionsController < ApplicationController 
-  
+class Api::Quiz::QuestionsController < ApplicationController
   def index
     respond_with Question.all, each_serializer: QuestionSerializer
   end
-  
+
   def create
     respond_with :api, :quiz, Question.create(question_params)
   end
-  
+
   def destroy
     respond_with Question.destroy(params[:id])
   end
 
   def update
     q = Question.find(params['id'])
-    q.update_attributes(item_params)
-    respond_with item, json: q
+    q.update_attributes(question_params)
+    respond_with q, json: q
   end
-  
-  private 
+
+  private
+
   def question_params
     params.require(:question).permit(:id, :question, :answer)
   end
